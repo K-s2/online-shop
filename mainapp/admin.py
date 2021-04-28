ceb0ce7ead3f9450507293f67c440d4d485ea4e7
K-s2 <81283622+K-s2@users.noreply.github.com>
2021-04-28 21:14:53 +0300

    Update admin.py

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,22 +1,39 @@
-from django import forms
-from django.forms import ModelChoiceField
+from django.core.exceptions import ValidationError
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import *
+from PIL import Image
 
 
-class TombstoneStrictFormCategoryChoiceField(forms.ModelChoiceField):
+class FenceAdminForm(ModelForm):
 
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, ** kwargs)
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; front-size:14px;">Загружайте изображения с минимальным разрешением {}x{}</span>'.format(
+                *Product.MIN_RESOLUTION
+            )
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображение меньше минимального!')
+        max_height, max_width = Product.MAX_RESOLUTION
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение изображение больше максимального!')
+        return image
 
 
-class TombstoneStrictFormAdmin(admin.ModelAdmin):
+class FenceAdmin(admin.ModelAdmin):
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'category':
-            return ModelChoiceField(Category.objects.filter(slug ='tombstonestricform'))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    form = FenceAdminForm
 
 
+admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(TombstoneStrictForm)
 admin.site.register(TombstoneUnusualForm)
