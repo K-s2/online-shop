fc9a4d128e77e5f697a7970bef07060d6468a187
K-s2 <81283622+K-s2@users.noreply.github.com>
2021-06-03 19:43:35 +0300

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,2 +1,8 @@
-from django.urls import path
-urlpatterns = []
+from django.urls import path, include
+from .views import test_view, ProductDetailVeiw
+
+urlpatterns = [
+    path('', test_view, name='base'),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailVeiw.as_view(), name='product_detail')
+
+]
